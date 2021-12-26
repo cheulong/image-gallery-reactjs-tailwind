f20b7a3a22d92280522c2a4f239a192fca08e74c
Cheulong Sear <searcheulong@gmail.com>
2021-12-26 12:33:52 +0700

    finish

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  mode: "jit",
+  purge: [
+    './public/**/*.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
