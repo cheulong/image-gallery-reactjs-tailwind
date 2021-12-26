17acf8244ea4b1f590c4a9116a8f7c3b670d1990
Cheulong Sear <searcheulong@gmail.com>
2021-12-26 12:34:42 +0700

    remove console

--- src/App.js
+++ src/App.js
@@ -8,8 +8,6 @@ function App() {
   const [term, setTerm] = useState('');
 
   useEffect(() => {
-    console.log('hii', process.env.REACT_APP_PIXABAY_API_KEY);
-
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
       .then(data => {
